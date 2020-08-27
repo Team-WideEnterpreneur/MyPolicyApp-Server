8ef01d747767be2ce6eb15b205a5388da53670e8
User <hjy5421@naver.com>
2020-08-27 14:38:21 +0900

    init

---
+++ src/main/java/com/dataproject/mydata/MydataApplication.java
@@ -0,0 +1,13 @@
+package com.dataproject.mydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydataApplication.class, args);
+    }
+
+}
